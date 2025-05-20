b4d97f5facd129d2806a771246de4338980b249f
knev <knev@users.noreply.github.com>
2025-05-20 10:05:40 +0200

    feat: remove singleton for thread safety; use background loop;

--- IPSME_MsgEnv.cpp
+++ IPSME_MsgEnv.cpp
@@ -3,13 +3,12 @@
 #include <assert.h>
 #include <string.h>
 #include <memory>
-#include <mutex>
 // #include <iostream>
 #include <unordered_map>
 
 #include "IPSME_MsgEnv.h"
-using IPSME_MsgEnv::RET_TYPE;
-using IPSME_MsgEnv::tp_callback;
+using RET_TYPE = IPSME_MsgEnv::RET_TYPE;
+using tp_callback = IPSME_MsgEnv::tp_callback;
 
 const char* psz_channel_pattern_ = "IPSME";
 
@@ -21,6 +20,7 @@ const int i_server_port = 1883;
 //----------------------------------------------------------------------------------------------------------------
 // singleton
 
+/*
 void message_callback_(struct mosquitto* mosq, void* obj, const struct mosquitto_message* message);
 
 class Singleton {
@@ -70,6 +70,7 @@ public: // private:
 };
 
 std::unique_ptr<Singleton> Singleton::_instance;
+*/
 
 //----------------------------------------------------------------------------------------------------------------
 
@@ -78,10 +79,11 @@ std::mutex mutex_vec_;
 
 void message_callback_(struct mosquitto* mosq, void* UNUSED, const struct mosquitto_message* p_message)
 {
-    std::lock_guard<std::mutex> lock(mutex_vec_);
+    // IPSME_MsgEnv* env = static_cast<IPSME_MsgEnv*>(UNUSED);
 
     // printf("%s: \n", __func__); fflush(stdout);
 
+    std::lock_guard<std::mutex> lock(mutex_vec_);
     for (const auto& pair : vec_) {
         tp_callback p_callback= (tp_callback)pair.first;
         void* p_void= pair.second;
@@ -94,6 +96,41 @@ void message_callback_(struct mosquitto* mosq, void* UNUSED, const struct mosqui
         }
     }
 }
+
+//----------------------------------------------------------------------------------------------------------------
+
+IPSME_MsgEnv::IPSME_MsgEnv() : _uptr_mosq(mosquitto_new(NULL, true, this), mosquitto_destroy)
+{
+    mosquitto_message_callback_set(_uptr_mosq.get(), message_callback_);
+
+    int delay = 1;  // Initial delay in seconds
+    const int max_delay = 60;  // Maximum delay in seconds
+
+    while (true) {
+        int connect_result = mosquitto_connect(_uptr_mosq.get(), "localhost", 1883, 60);
+        if (connect_result == MOSQ_ERR_SUCCESS)
+            break;
+
+        // std::cerr << "Mosquitto connect failed: " << mosquitto_strerror(connect_result) << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(delay));
+        delay *= 2;
+
+        if (delay > max_delay)
+            delay = max_delay;
+    }
+    // Optionally start the background loop
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
+    mosquitto_loop_start(_uptr_mosq.get());
+}
+
+IPSME_MsgEnv::~IPSME_MsgEnv()
+{
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
+    mosquitto_loop_stop(_uptr_mosq.get(), true);
+}
+
+//----------------------------------------------------------------------------------------------------------------
 
 RET_TYPE IPSME_MsgEnv::subscribe(tp_callback p_callback, void* p_void)
 {
@@ -109,8 +146,8 @@ RET_TYPE IPSME_MsgEnv::subscribe(tp_callback p_callback, void* p_void)
         vec_.emplace_back(p_callback, p_void);
     }
 
-    Singleton& s = Singleton::getInstance();
-    int ret= mosquitto_subscribe(s._uptr_mosq.get(), NULL, psz_channel_pattern_, 0);
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
+    int ret= mosquitto_subscribe(_uptr_mosq.get(), NULL, psz_channel_pattern_, 0);
     if (ret)
         return ret;
 
@@ -119,8 +156,8 @@ RET_TYPE IPSME_MsgEnv::subscribe(tp_callback p_callback, void* p_void)
 
 RET_TYPE IPSME_MsgEnv::unsubscribe(tp_callback p_callback)
 {
-    Singleton& s = Singleton::getInstance();
-    int ret= mosquitto_unsubscribe(s._uptr_mosq.get(), NULL, psz_channel_pattern_);
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
+    int ret= mosquitto_unsubscribe(_uptr_mosq.get(), NULL, psz_channel_pattern_);
     if (ret)
         return ret;
 
@@ -143,9 +180,8 @@ RET_TYPE IPSME_MsgEnv::publish(MSG_TYPE msg)
 {
     // printf("%s: \n", __func__); fflush(stdout);
 
-    Singleton& s = Singleton::getInstance();
-
-    int ret = mosquitto_publish(s._uptr_mosq.get(), NULL, psz_channel_pattern_, (int) strlen(msg), msg, 0, false);
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
+    int ret = mosquitto_publish(_uptr_mosq.get(), NULL, psz_channel_pattern_, (int) strlen(msg), msg, 0, false);
     if (ret) {
         // std::cerr << "Can't publish to topic\n";
         return ret;
@@ -154,10 +190,12 @@ RET_TYPE IPSME_MsgEnv::publish(MSG_TYPE msg)
     return 0;
 }
 
+/*
 RET_TYPE IPSME_MsgEnv::process_requests(int i_timeout)
 {
-    Singleton& s = Singleton::getInstance();
+    std::lock_guard<std::mutex> lock(_mutex_mosq);
     mosquitto_loop(s._uptr_mosq.get(), i_timeout, 1);
 
     return 0;
 }
+*/
--- IPSME_MsgEnv.h
+++ IPSME_MsgEnv.h
@@ -7,23 +7,30 @@
 #define IPSME_MSGENV_H
 
 #include <mosquitto.h>
+#include <mutex>
 
 // rather than include an init() in the interface
 // we assume the dev knows we are using mosquitto and calls mosquitto_lib_init() and mosquitto_lib_cleanup()
 
-namespace IPSME_MsgEnv
-{
+class IPSME_MsgEnv {
+public:
 	typedef int RET_TYPE;
 	typedef char const * const MSG_TYPE;
 
 	typedef void (*tp_callback)(MSG_TYPE, void* p_void);
+
+public:
+	IPSME_MsgEnv();
+	~IPSME_MsgEnv();
 
 	RET_TYPE subscribe(tp_callback p_callback, void* p_void);
 	RET_TYPE unsubscribe(tp_callback p_callback);
 
 	RET_TYPE publish(MSG_TYPE);
 
-	RET_TYPE process_requests(int i_timeout= 0);
-}
+private:
+	std::unique_ptr<struct mosquitto, decltype(&mosquitto_destroy)> _uptr_mosq;
+	std::mutex _mutex_mosq;
+};
 
 #endif // IPSME_MSGENV_H
